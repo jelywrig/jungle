1f6e606034c1ca26af069821852f6f207ca50a44
Jessie Wright <jessie.wright@gmail.com>
2020-06-01 20:50:24 +0000

    updated test desc

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
     it 'saves succesfully when all required fields present' do
-      category=Category.new(name:"Test")
-      category.save
+
       product=Product.new(name: "Testing",
       description: "Test text blah blah blah",
-      category_id: category.id,
+      category_id: 1,
       quantity: 5,
       price: 500)
+      
       expect(product).to be_valid
 
     end
@@ -42,7 +42,7 @@ RSpec.describe Product, type: :model do
       product.valid?
       expect(product.errors[:quantity]).to include ("can't be blank")
     end
-    it 'throws "can\'t be blank" error when quantity not present' do
+    it 'throws "can\'t be blank" error when category not present' do
       product=Product.new( name: 'Test',
       description: "Test text blah blah blah",
       quantity: 5,
